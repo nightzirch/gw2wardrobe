7fa0fe6b6893090dd094540a6d026f1f9b3951f5
nightzirch <chisse1049@hotmail.com>
2014-04-10 13:59:16 +0200

    A good second step

--- index.php
+++ index.php
@@ -22,16 +22,49 @@
 	</head>
 	
 	<body>
+		<nav class="navbar navbar-default" role="navigation">
+			<div class="container-fluid">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">Guild Wars 2 Wardrobe <span class="badge badge-danger">ALPHA</span></a>
+				</div>
+				
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="">Wardrobe</a></li>
+						<li><a href="about.php">About</a></li>
+						<li><a href="changelog.php">Changelog</a></li>
+					</ul>
+				</div><!-- /.navbar-collapse -->
+			</div><!-- /.container-fluid -->
+		</nav>
+		
 		<div class="container-fluid">
+			<!-- Danger alert -->
+			<div class="alert alert-danger">
+				<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+				<h4>Guild Wars 2 Wardrobe is in alpha!</h4>
+				Hello and thanks for checking out the site. I would like to inform you that Guild Wars 2 Wardrobe is under development and that there are currently only a few features available. However, feel free to play around. If you want more information about the site, you can read about planned <a class="alert-link" href="features.php">upcoming features</a> or check out <a class="alert-link" href="changelog.php">the changelog</a>.
+			</div>
+			
 			<div class="row">
 				<div class="col-md-2">
 					<div id="sortMenu" class="panel-group">
 						<div class="panel panel-default">
 							<div class="panel-heading">
-								<a href="#sortArmor" data-toggle="collapse" data-parent="#sortMenu">
-									Armor Skins
-									<span class="collapse-arrow span-right">&#x25B2;</span>
-								</a>
+								<h5>
+									<a href="#sortArmor" data-toggle="collapse" data-parent="#sortMenu">
+										Armor Skins
+										<span class="collapse-arrow pull-right">&#x25B2;</span>
+									</a>
+								</h5>
 							</div>
 							
 							<ul id="sortArmor" class="list-group collapse in">
@@ -44,10 +77,12 @@
 						
 						<div class="panel panel-default">
 							<div class="panel-heading">
-								<a href="#sortWeapon" class="collapsed" data-toggle="collapse" data-parent="#sortMenu">
-									Weapon Skins
-									<span class="collapse-arrow span-right">&#x25B2;</span>
-								</a>
+								<h5>
+									<a href="#sortWeapon" class="collapsed" data-toggle="collapse" data-parent="#sortMenu">
+										Weapon Skins
+										<span class="collapse-arrow pull-right">&#x25B2;</span>
+									</a>
+								</h5>
 							</div>
 							
 							<ul id="sortWeapon" class="list-group collapse">
@@ -75,10 +110,12 @@
 						
 						<div class="panel panel-default">
 							<div class="panel-heading">
-								<a href="#sortOutfit" class="collapsed" data-toggle="collapse" data-parent="#sortMenu">
-									Outfits
-									<span class="collapse-arrow span-right">&#x25B2;</span>
-								</a>
+								<h5>
+									<a href="#sortOutfit" class="collapsed" data-toggle="collapse" data-parent="#sortMenu">
+										Outfits
+										<span class="collapse-arrow pull-right">&#x25B2;</span>
+									</a>
+								</h5>
 							</div>
 							
 							<ul id="sortOutfit" class="list-group collapse">
@@ -91,32 +128,96 @@
 					</div>
 				</div>
 				
-				<div class="col-md-8">
+				<div class="col-md-7">
+					<div id="loading" data-bind="visible: loading">
+						<img src="img/loading.gif" alt="Loading" />
+					</div>
+					
 					<div id="itemsContainer" class="panel-group" data-bind="foreach: armors">
 						
 						<div class="panel panel-default">
 							<div class="panel-heading">
-								<a href="#lightChest" data-toggle="collapse" data-parent="#itemsContainer" data-bind="text: name">
-									<span class="collapse-arrow span-right">&#x25B2;</span>
-								</a>
+								<a data-toggle="collapse" data-bind='html: name + " (" + value.length + ")" + arrow, attr: {href: "#" + id}'></a>
 							</div>
 							
-							<!--<div id="lightChest" class="panel-collapse collapse in">
-								<div class="panel-body" data-bind="foreach: armors">
+							<div class="panel-collapse collapse in" data-bind="attr: {id: id}">
+								<div class="panel-body" data-bind="foreach: value">
 									<div class="itemBlock" data-bind='attr: {"data-gw2item": id}'>
 										<img data-bind="attr: {src: img}" />
 									</div>
 								</div>
-							</div>-->
+							</div>
+							
+							<!-- ko if: $index() == 0 -->
+								<!--<div class="panel-heading">
+									<a data-toggle="collapse" data-bind='html: name + arrow, attr: {href: "#" + id}'></a>
+								</div>
+								
+								<div class="panel-collapse collapse in" data-bind="attr: {id: id}">
+									<div class="panel-body" data-bind="foreach: value">
+										<div class="itemBlock" data-bind='attr: {"data-gw2item": id}'>
+											<img data-bind="attr: {src: img}" />
+										</div>
+									</div>
+								</div>-->
+							<!-- /ko -->
+							
+							<!-- ko if: $index() != 0 -->
+								<!--<div class="panel-heading">
+									<a data-toggle="collapse" class="collapsed" data-bind='html: name + arrow, attr: {href: "#" + id}'></a>
+								</div>
+								
+								<div class="panel-collapse collapse" data-bind="attr: {id: id}">
+									<div class="panel-body" data-bind="foreach: value">
+										<div class="itemBlock" data-bind='attr: {"data-gw2item": id}'>
+											<img data-bind="attr: {src: img}" />
+										</div>
+									</div>
+								</div>-->
+							<!-- /ko -->
 						</div>
-						
-						
-						
-						
 					</div>
 				</div>
-				<div class="col-md-2">
-				
+				<div class="col-md-3">
+					<div id="itemDetails">
+						<div class="panel panel-default">
+							<div class="panel-heading">
+								<h5>Item Details</h5>
+							</div>
+							
+							<div class="panel-body">
+								<img class="detailIcon pull-left" data-bind="attr: {src: detailIcon}" />
+								<h5 class="detailName" data-bind="text: detailName"></h5>
+								<p class="detailDesc text-muted" data-bind="text: detailDesc"></p>
+								
+								<div data-bind="visible: detailAcquire">
+									<h5>Acquisition</h5>
+									<div data-bind="html: detailAcquire"></div>
+								</div>
+								
+								<div data-bind="visible: detailRecipe">
+									<h5>Recipe</h5>
+									<div data-bind="html: detailRecipeText"></div>
+								</div>
+								
+								<button type="button" class="btn btn-primary" id="trackerAdd" data-bind='attr: {"data-id": detailId}'>Add to tracker</button>
+								<a class="btn btn-default" role="button" data-bind="attr: {href: detailPage}">Official Wiki</a>
+								
+							</div>
+						</div>
+					</div>
+					
+					<div id="tracker">
+						<div class="panel panel-default">
+							<div class="panel-heading">
+								<h5>Item Tracker</h5>
+							</div>
+							
+							<div class="panel-body">
+								
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
--- js/script.js
+++ js/script.js
@@ -12,20 +12,62 @@ $(document).ready(function() {
 
 var gw2w = {
 	ready: function() {
-		// Populate
-		//gw2w.populate.items();
-		gw2w.populate.types();
-		gw2w.populate.armors();
-		gw2w.populate.weapons();
-		
-		// Plugins
-		gw2w.plugins.tooltip();
-		
 		// Creates a view model object from the class
 		gw2w.vm = new gw2w.viewModel();
 		
 		// Applies the view model
 		ko.applyBindings(gw2w.vm);
+		
+		// Fetch ALL the stuff o/
+		$.when(
+			// Start loading
+			gw2w.loading(true),
+			
+			// Types
+			$.ajax({
+				dataType: "json",
+				url: gw2w.api.gw2spidy.types(),
+				success: function(data) {
+					console.log(data);
+					gw2w.items.types = data;
+					gw2w.process.types();
+				}
+			}),
+			
+			// Armors
+			$.ajax({
+				dataType: "json",
+				url: gw2w.api.gw2spidy.allArmors(),
+				success: function(data) {
+					console.log(data);
+					gw2w.items.armors = data;
+					gw2w.process.armors();
+				}
+			}),
+			
+			// Weapons
+			$.ajax({
+				dataType: "json",
+				url: gw2w.api.gw2spidy.allWeapons(),
+				success: function(data) {
+					console.log(data);
+					gw2w.items.weapons = data;
+					gw2w.process.weapons();
+				}
+			})
+		).then(function() {
+			// Stop loading
+			gw2w.loading(false),
+				
+			// Update
+			gw2w.update();
+			
+			// Plugins
+			gw2w.plugins.tooltip();
+			
+			// Listeners
+			gw2w.listeners.all();
+		});
 	},
 	
 	// The view model object
@@ -35,19 +77,47 @@ var gw2w = {
 	viewModel: function() {
 		var self = this;
 		
+		// Loading
+		self.loading = ko.observable(false);
+		
 		// Armor and weapon array
 		self.armors = ko.observableArray();
 		self.weapons = ko.observableArray();
 		self.armorSubtypes = ko.observableArray();
 		self.weaponSubtypes = ko.observableArray();
+		
+		// Item details
+		self.detailId = ko.observable();
+		self.detailName = ko.observable();
+		self.detailDesc = ko.observable();
+		self.detailIcon = ko.observable();
+		self.detailImage = ko.observable();
+		self.detailPage = ko.observable();
+		self.detailAcquire = ko.observable();
+		self.detailRecipe = ko.observable(false);
+		
+		self.detailRecipeText = ko.computed(function(){
+			var text = 'This item is created through a recipe. Check out <a href="' + self.detailPage() + '">the Official Wiki</a> for the recipe.';
+			return text;
+		});
+		
 	},
 	
 	api: {
 		items: function() {
 			return "https://api.guildwars2.com/v1/items.json";
 		},
-		itemDetails: function() {
-			return "https://api.guildwars2.com/v1/item_details.json";
+		itemDetails: function(id) {
+			return "https://api.guildwars2.com/v1/item_details.json?item_id=" + id;
+		},
+		render: function(sig, id) {
+			return "https://render.guildwars2.com/file/" + sig + "/" + id + ".jpg";
+		},
+		itemPage: function(name) {
+			// Getting rid of spaces in the name
+			name = gw2w.general.replace(" ", "_", name);
+			
+			return "http://wiki.guildwars2.com/wiki/" + name;
 		},
 		gw2spidy: {
 			types: function() {
@@ -59,6 +129,15 @@ var gw2w = {
 			allWeapons: function() {
 				return "http://www.gw2spidy.com/api/v0.9/json/all-items/18";
 			}
+		}
+	},
+	
+	general: {
+		unspacify: function(str) {
+			return str.split(" ").join("");
+		},
+		replace: function(find, replace, str) {
+			return str.replace(new RegExp(find, 'g'), replace);
 		}
 	},
 	
@@ -95,6 +174,18 @@ var gw2w = {
 		}
 	},
 	
+	listeners: {
+		all: function() {
+			gw2w.listeners.itemBlock();
+		},
+		itemBlock: function() {
+			$(".itemBlock").on("click", function() {
+				var id = $(this).attr("data-gw2item");
+				gw2w.populate.itemDetails(id);
+			});
+		}
+	},
+	
 	populate: {
 		items: function() {
 			var url = gw2w.api.items();
@@ -111,25 +202,16 @@ var gw2w = {
 				}
 			});
 		},
-		details: function() {
-			$.each(gw2w.items.raw.items, function(index, value) {
-				var id = value;
-				var url = gw2w.api.itemDetails();
-				url = url + "?item_id=" + id;
-				var arr = new Array();
-				
-				console.log(id);
-				
-				$.ajax({
-					dataType: "json",
-					url: url,/*
-					data: {
-						item_id: id
-					},*/
-					success: function(data) {
-						arr.push(data);
-					}
-				});
+		itemDetails: function(id) {
+			var url = gw2w.api.itemDetails(id);
+			
+			$.ajax({
+				dataType: "json",
+				url: url,
+				success: function(data) {
+					gw2w.items.details = data;
+					gw2w.process.itemDetails();
+				}
 			});
 		},
 		types: function() {
@@ -170,6 +252,20 @@ var gw2w = {
 					gw2w.process.weapons();
 				}
 			});
+		},
+		itemPage: function(name) {
+			var url = gw2w.api.itemPage(name);
+			
+			$.ajax({
+				dataType: "html",
+				url: "get.php",
+				data: "url=" + url,
+				success: function(data) {
+					console.log(data);
+					gw2w.items.page = data;
+					gw2w.process.itemPage();
+				}
+			});
 		}
 	},
 	
@@ -183,7 +279,7 @@ var gw2w = {
 				
 				// Lets organize the armor types.
 				// We place the object into a multidimentional array.
-				vm.armors()[i]().value().push(new gw2w.class.armor(obj));
+				vm.armors()[i].value.push(new gw2w.class.armor(obj));
 			});
 		},
 		weapons: function() {
@@ -195,7 +291,7 @@ var gw2w = {
 				
 				// Lets organize the weapon types.
 				// We place the object into a multidimentional array.
-				vm.weapons()[i]().value().push(new gw2w.class.weapon(obj));
+				vm.weapons()[i].value.push(new gw2w.class.weapon(obj));
 			});
 		},
 		types: function() {
@@ -218,21 +314,120 @@ var gw2w = {
 			// Creating multidimentional arrays inside gw2w.vm.armors and gw2w.vm.weapons
 			$.each(armorSubtypes, function(index, obj) {
 				//vm.armors()[obj.id] = {"name": obj.name, "value": ko.observableArray()};
-				vm.armors()[obj.id] = ko.observableArray([{"name": obj.name}, {"value": ko.observableArray()}]);
+				vm.armors()[obj.id] = {"id": gw2w.general.unspacify(obj.name), "name": obj.name, "arrow": gw2w.items.arrow, "value": new Array()};
 			});
 			$.each(weaponSubtypes, function(index, obj) {
 				//vm.weapons()[obj.id] = {"name": obj.name, "value": ko.observableArray()};
-				vm.weapons()[obj.id] = ko.observableArray([{"name": obj.name}, {"value": ko.observableArray()}]);
+				vm.weapons()[obj.id] = {"id": gw2w.general.unspacify(obj.name), "name": obj.name, "arrow": gw2w.items.arrow, "value": new Array()};
 			});
+		},
+		itemDetails: function() {
+			var vm = gw2w.vm;
+			var details = gw2w.items.details;
+			
+			// Clean the variables
+			vm.detailId(null);
+			vm.detailName(null);
+			vm.detailDesc(null);
+			vm.detailIcon(null);
+			vm.detailImage(null);
+			vm.detailPage(null);
+			vm.detailAcquire(null);
+			vm.detailRecipe(false);
+			
+			
+			// Fetch item details from the official wiki
+			gw2w.populate.itemPage(details.name);
+			
+			vm.detailId(details.item_id);
+			vm.detailName(details.name);
+			vm.detailDesc(details.description);
+			vm.detailIcon(gw2w.api.render(details.icon_file_signature, details.icon_file_id));
+			vm.detailImage();
+			vm.detailPage(gw2w.api.itemPage(details.name));
+//			vm.detailAcquire(); // Done automagically
+//			vm.detailRecipe();	// Done automagically
+		},
+		itemPage: function() {
+			var vm = gw2w.vm;
+			var page = gw2w.items.page;
+			
+			// If successful
+			if(page) {
+				// Convert the page contents from a string to elements
+				page = $(page);
+				
+				// Locate the content about acquisition
+				var acq = $("#Acquisition", page);
+				
+				// If the page has a section about acquisition
+				if(acq) {
+					// Find the text about how to acquire
+					acq = $(acq).parent().next();
+					
+					// Convert from jQuery element to pure JS to new String
+					acq = $(acq)[0].outerHTML;
+					
+					console.log(acq);
+					
+					var vm = gw2w.vm;
+					vm.detailAcquire(acq);
+				}
+				
+				// If not
+				else {
+					// Locate the recipe
+					var recipe = $("#Recipes", page);
+					
+					// If there is a recipe
+					if(recipe) {
+						// Set recipe to true
+						vm.detailRecipe(true);
+					}
+				}
+			}
+			
+			// Error handling, if target page does not exist
+			else {
+				
+			}
 		}
 	},
 	
 	items: {
-		list: null,
+		details: null,
+		page: null,
 		raw: null,
 		types: null,
 		armors: null,
-		weapons: null
+		weapons: null,
+		arrow: "<span class='collapse-arrow pull-right'>&#x25B2;</span>"
+	},
+	
+	update: function() {
+		gw2w.vm.armors.push();
+		gw2w.vm.weapons.push();
+	},
+	
+	loading: function(state) {
+		var vm = gw2w.vm;
+		
+		switch(state) {
+			// Lets turn on loading
+			case true:
+				vm.loading(true);
+				break;
+			
+			// Lets turn off loading
+			case false:
+				vm.loading(false);
+				break;
+			
+			// Error, unexpected parameter
+			default:
+				
+				break;
+		}
 	},
 	
 	plugins: {
