04010aeea47676e09c2a24c81d1d3fc590a4a2ab
nightzirch <chisse1049@hotmail.com>
2015-05-23 23:52:15 +0200

    Public profile now also displays characters

--- routes/views/user.js
+++ routes/views/user.js
@@ -1,12 +1,17 @@
 var keystone = require('keystone'),
-	async = require('async');
+	async = require('async'),
+	request = require('request');
 
 exports = module.exports = function(req, res) {
 	var view = new keystone.View(req, res),
 		locals = res.locals;
 	
+	var successfulRequests = 0;
+	var allRequests = 1;
+	
 	locals.username = req.params.username;
 	locals.title = locals.username;
+	locals.characters = null;
 
 	var q = keystone.list('User').model.findOne({
 		username: locals.username
@@ -16,19 +21,44 @@ exports = module.exports = function(req, res) {
 	
 	locals.section = "users";
 	
-	view.on('init', function(next) {
-		q2.exec(function(err2, result2) {
-			locals.allSkins = result2;
+	q.exec(function(err, result) {
+		locals.profile = result;
 
-			
-			q.exec(function(err, result) {
-				locals.profile = result;
+		// Lets fetch /v2/characters from the API
+		if(locals.profile.token) {
+			request.get('https://api.guildwars2.com/v2/characters?page=0&page_size=200', {
+				'auth': {
+					'bearer': locals.profile.token
+				}
+			}, function(error, response, body) {
+				if(error) {
+					locals.characters = error;
+				} else {
+					locals.characters = JSON.parse(body);
+					console.log(locals.characters);
+				}
 
-				next(err);
+				requestCallback();
 			});
-		});
+		} else {
+			requestCallback();
+		}
 	});
 	
-	// Render the view
-	view.render('user');
+	function requestCallback() {
+		successfulRequests++;
+		
+		if(successfulRequests == allRequests) {
+			view.on('init', function(next) {
+				q2.exec(function(err2, result2) {
+					locals.allSkins = result2;
+
+					next(err2);
+				});
+			});
+			
+			// Render the view
+			view.render('user');
+		}
+	}
 };
